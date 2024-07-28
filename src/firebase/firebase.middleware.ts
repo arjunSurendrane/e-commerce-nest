66328979fc96ede5d987d6c4f1136dd601208004
Arjun E <arjun.e@astriadigital.com>
2024-07-28 15:11:48 +0530

    added firebase initialised app and verify token in middleware

--- src/firebase/firebase.middleware.ts
+++ src/firebase/firebase.middleware.ts
@@ -1,10 +1,31 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
-import { Request, Response, NextFunction } from 'express';
+import { Response, NextFunction } from 'express';
+import { getAuth } from 'firebase-admin/auth';
+import firebaseAppConfig from './firebase.config';
+import { CustomRequest } from 'src/types/express';
 
 @Injectable()
 export class FirebaseMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
-    console.log(`Request...`, req);
-    next();
+  async use(req: CustomRequest, res: Response, next: NextFunction) {
+    try {
+      const defaultAuth = getAuth(firebaseAppConfig);
+      const BearerToken = req.headers?.authentication as string | undefined;
+      if (!BearerToken) {
+        console.log('error token not exist');
+        return;
+      }
+      const token = BearerToken.split(' ')[0];
+      const decodeToken = await defaultAuth.verifyIdToken(token);
+      if (!decodeToken) {
+        console.log('user not authenticated');
+        return;
+      }
+      req.user = decodeToken;
+      console.log(`Request...`, req);
+      next();
+    } catch (error) {
+      console.log('ERROR:', error);
+      return;
+    }
   }
 }
